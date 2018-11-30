fcca3fb6904adc39c00ea40129a6684c79357f3c
Maikel van Veen <maikeloo10@hotmail.com>
2018-11-30 16:54:15 +0100

    feat(App): add first development base for the app

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import 'babel-polyfill'
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import { RootView } from './views/RootView'
+
+const App: React.SFC = () => (
+    <RootView />
+)
+
+const rootElement = document.getElementById('react-root')
+
+ReactDOM.render(<App />, rootElement)
---
+++ client/src/views/RootView.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+interface Props {}
+
+export class RootView extends React.Component<Props> {
+    public render() {
+        return (
+            <div className={`asa-RootView`}>
+                Hello
+            </div>
+        )
+    }
+}
