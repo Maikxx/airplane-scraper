27b5f7bb7923e1648ba3ab48b0140283956fa90c
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-07 14:23:45 +0100

    fix(search): fix lowercase not finding results

--- api/controllers/airplane.ts
+++ api/controllers/airplane.ts
@@ -30,7 +30,7 @@ export const get = (req: express.Request, res: express.Response, next: express.N
 export const search = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const page = req.query.page !== undefined && parseInt(req.query.page, 10) || 0
     const limit = req.query.limit !== undefined && parseInt(req.query.limit, 10) || 20
-    const search = req.query.searchText
+    const search = req.query.searchText && `${req.query.searchText[0].toUpperCase()}${req.query.searchText.slice(1)}`
 
     if (!search) {
         res.status(204).json({ message: 'No content' })
