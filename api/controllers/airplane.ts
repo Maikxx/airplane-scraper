53699faab4a9baf0702c2ca1d0aa02c15e6211c8
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-09 18:37:04 +0100

    fix(airplane): add more stable way to add dynamic properties

--- api/controllers/airplane.ts
+++ api/controllers/airplane.ts
@@ -6,9 +6,9 @@ export const get = (req: express.Request, res: express.Response, next: express.N
     const limit = req.query.limit !== undefined && parseInt(req.query.limit, 10) || 20
     const search = req.query.searchText && `${req.query.searchText[0].toUpperCase()}${req.query.searchText.slice(1)}`
 
-    const filters = search
-        ? { title: { $regex: search }}
-        : {}
+    const filters = {
+        ...(!!search && { title: { $regex: search }}),
+    }
 
     Airplane.countDocuments(filters, (err, count) => {
         Airplane.find(filters)
