76d26615b6bff06b3a799cefda78280f397acd6e
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-09 18:28:47 +0100

    fix(router): fix routing error

--- api/routes/airplanes.ts
+++ api/routes/airplanes.ts
@@ -3,7 +3,6 @@ import * as AirplaneController from '../controllers/airplane'
 
 const router = express.Router()
 
-router.get('/search', AirplaneController.search)
 router.get('/', AirplaneController.get)
 
 export default router
