2bcaa893f2635f26e1400398af3e4662b1452a6e
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-09 18:28:01 +0100

    fix(Airplane): no longer use alternate url for search

--- api/controllers/airplane.ts
+++ api/controllers/airplane.ts
@@ -4,40 +4,14 @@ const Airplane = require('../models/airplane')
 export const get = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const page = req.query.page !== undefined && parseInt(req.query.page, 10) || 0
     const limit = req.query.limit !== undefined && parseInt(req.query.limit, 10) || 20
-
-    Airplane.countDocuments({}, (err, count) => {
-        Airplane.find()
-            .skip(page * limit)
-            .limit(limit)
-            .exec((err, docs) => {
-                if (err) {
-                    res.status(500).json(err)
-                    return
-                }
-
-                const hasNextPage = page < count / 20
-                const response = {
-                    totalCount: count,
-                    hasNextPage,
-                    nodes: docs,
-                }
-
-                res.status(200).json(response)
-            })
-    })
-}
-
-export const search = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const page = req.query.page !== undefined && parseInt(req.query.page, 10) || 0
-    const limit = req.query.limit !== undefined && parseInt(req.query.limit, 10) || 20
     const search = req.query.searchText && `${req.query.searchText[0].toUpperCase()}${req.query.searchText.slice(1)}`
 
-    if (!search) {
-        res.status(204).json({ message: 'No content' })
-    }
+    const filters = search
+        ? { title: { $regex: search }}
+        : {}
 
-    Airplane.countDocuments({ title: { $regex: search }}, (err, count) => {
-        Airplane.find({ title: { $regex: search }})
+    Airplane.countDocuments(filters, (err, count) => {
+        Airplane.find(filters)
             .skip(page * limit)
             .limit(limit)
             .exec((err, docs) => {
