dae09919523dda4e4743c3cdf35bc5cde7e7726c
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-11 15:20:24 +0100

    fix(migrations): implement full runMigrations function

--- api/db/migrations.ts
+++ api/db/migrations.ts
@@ -1,5 +1,9 @@
+import { migrateAirplaneOrigins } from './migrations/origin'
+import { migrateAirplaneRoles } from './migrations/roles'
+import { migrateAirplaneManufacturers } from './migrations/manufacturer'
+
 export const runMigrations = async () => {
-    // await migrateAirplaneOrigins()
-    // await migrateAirplaneRoles()
-    // await migrateAirplaneManufacturers()
+    await migrateAirplaneOrigins()
+    await migrateAirplaneRoles()
+    await migrateAirplaneManufacturers()
 }
