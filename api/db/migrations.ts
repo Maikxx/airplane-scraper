8eba8cd0608316f4c703a80cf4445527407c860d
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-12 15:30:40 +0100

    fix(migrations): prevent migrations from running by default

--- api/db/migrations.ts
+++ api/db/migrations.ts
@@ -1,13 +1,13 @@
-// import { migrateAirplaneOrigins } from './migrations/origin'
-// import { migrateAirplaneRoles } from './migrations/roles'
-// import { migrateAirplaneManufacturers } from './migrations/manufacturer'
-// import { migrateAirplaneUsageStatus } from './migrations/usageStatus'
+import { migrateAirplaneOrigins } from './migrations/origin'
+import { migrateAirplaneRoles } from './migrations/roles'
+import { migrateAirplaneManufacturers } from './migrations/manufacturer'
+import { migrateAirplaneUsageStatus } from './migrations/usageStatus'
 import { migrateAirplaneAmountBuilt } from './migrations/amountBuilt'
 
 export const runMigrations = async () => {
-    // await migrateAirplaneOrigins()
-    // await migrateAirplaneRoles()
-    // await migrateAirplaneManufacturers()
-    // await migrateAirplaneUsageStatus()
+    await migrateAirplaneOrigins()
+    await migrateAirplaneRoles()
+    await migrateAirplaneManufacturers()
+    await migrateAirplaneUsageStatus()
     await migrateAirplaneAmountBuilt()
 }
