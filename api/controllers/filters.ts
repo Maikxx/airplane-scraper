7f3a76c2fe819d2d7489c05c06c00543081f4b88
Maikel van Veen <maikeloo10@hotmail.com>
2018-12-12 09:59:27 +0100

    feat(api): add support for getting usage statuses

--- api/controllers/filters.ts
+++ api/controllers/filters.ts
@@ -1,41 +1,67 @@
-import * as express from 'express'
+import { Request, Response, NextFunction } from 'express'
+import { MongoError } from 'mongodb'
 const Airplane = require('../models/airplane')
 
-export const getRoles = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const getRoles = (request: Request, response: Response, next: NextFunction) => {
     Airplane.find({ role: { $nin: [ undefined, null, '', '\n' ]}})
         .distinct('role')
-        .exec((err, roles) => {
+        .exec((err: MongoError, roles) => {
             if (err) {
-                res.status(500).json(err)
-                return null
+                return onError(err, response)
             }
 
-            res.status(200).json(roles)
+            response
+                .status(200)
+                .json(roles)
         })
 }
 
-export const getOrigins = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const getOrigins = (request: Request, response: Response, next: NextFunction) => {
     Airplane.find({ origin: { $nin: [ undefined, null, '', '\n' ]}})
         .distinct('origin')
-        .exec((err, origins) => {
+        .exec((err: MongoError, origins) => {
             if (err) {
-                res.status(500).json(err)
-                return null
+                return onError(err, response)
             }
 
-            res.status(200).json(origins)
+            response
+                .status(200)
+                .json(origins)
         })
 }
 
-export const getManufacturers = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+export const getManufacturers = (request: Request, response: Response, next: NextFunction) => {
     Airplane.find({ manufacturedBy: { $nin: [ undefined, null, '', '\n' ]}})
         .distinct('manufacturedBy')
-        .exec((err, manufacturers) => {
+        .exec((err: MongoError, manufacturers) => {
             if (err) {
-                res.status(500).json(err)
-                return null
+                return onError(err, response)
             }
 
-            res.status(200).json(manufacturers)
+            response
+                .status(200)
+                .json(manufacturers)
         })
+}
+
+export const getUsageStatuses = (request: Request, response: Response, next: NextFunction) => {
+    Airplane.find({ usageStatus: { $nin: [ undefined, null, '', '\n' ]}})
+        .distinct('usageStatus')
+        .exec((err: MongoError, usageStatuses) => {
+            if (err) {
+                return onError(err, response)
+            }
+
+            response
+                .status(200)
+                .json(usageStatuses)
+        })
+}
+
+const onError = (error: MongoError, response: Response) => {
+    response
+        .status(500)
+        .json(error)
+
+    return null
 }
